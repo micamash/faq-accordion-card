64828c959aa85f8acf36d2f514b20f23ff598cfc
Ashley Mical <ashleymmical@gmail.com>
2023-07-11 15:10:49 -0500

    updated JS to close expanded elements when another is clicked

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+function toggleCollapsible() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
+
+function closeOtherCollapsibles(currentCollapsible) {
+    for (i = 0; i < coll.length; i++) {
+        var content = coll[i].nextElementSibling;
+        if (coll[i] !== currentCollapsible && content.style.maxHeight) {
+            coll[i].classList.remove("active");
+            content.style.maxHeight = null;
+        }
+    }
+}
+
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
+        closeOtherCollapsibles(this);
+        toggleCollapsible.call(this);
+    });
+    coll[i].addEventListener("keydown", function (event) {
+        if (event.key === "Enter" || event.key === " ") {
+            event.preventDefault();
+            closeOtherCollapsibles(this);
+            toggleCollapsible.call(this);
         }
     });
 }
